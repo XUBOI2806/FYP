ffdb7de304f1e77e5da2ac1f3cd0d70be7ed147f
Seng Kuang Yap <syap0014@student.monash.edu>
2024-04-09 00:47:13 +1000

    targets shatter when ball collides

--- Assets/Scripts/Target/targetScript.cs
+++ Assets/Scripts/Target/targetScript.cs
@@ -1,20 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class targetScript : MonoBehaviour
 {
     GameObject target;
     public ScoreManager ScoreManager;
-    
-    private void OnTriggerEnter(Collider other)
+
+    public GameObject fractured;
+   
+
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.name == "Ball")
+        if (collision.gameObject.name == "Ball")
         {
             Debug.Log("ball hit the target!");
-            gameObject.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
+            //gameObject.GetComponent<Renderer>().material.color = new Color(0, 255, 0);
             ScoreManager.IncrementScore();
+
+            Vector3 oldPos = transform.position;
+            Instantiate(fractured, oldPos, Quaternion.identity);
+            Destroy(gameObject);
+
+            StartCoroutine(DestroyFracturedObjectAfterDelay(fractured, 1));
         }
     }
 
+
+    private IEnumerator DestroyFracturedObjectAfterDelay(GameObject obj, float delay)
+    {
+        Debug.Log("adfasdf");
+        yield return new WaitForSeconds(delay);
+
+
+        Destroy(obj);
+    }
 }

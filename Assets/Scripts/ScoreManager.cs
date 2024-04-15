92f1ff95aa864d957ae10528849f51ecc2548220
Daniel Ding <ddin0018@student.monash.edu>
2024-04-15 14:23:37 +1000

    soccer animations

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,11 +4,14 @@ using TMPro; // Namespace for TextMeshPro
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText; // Reference to the TextMeshProUGUI component
+    public TextMeshProUGUI missesText; // Reference to the TextMeshProUGUI component
     public static int score = 0; // To keep track of the score
+    public static int misses = 0; // To keep track of the score
 
     void Start()
     {
         UpdateScoreText(); // Initial score display
+        UpdateMissesText();
     }
 
     // Call this method every time a successful shot is made
@@ -18,9 +21,20 @@ public class ScoreManager : MonoBehaviour
         UpdateScoreText(); // Update the UI text
     }
 
+    public void IncrementMisses()
+    {
+        misses++; // Increase the score by 1
+        UpdateMissesText(); // Update the UI text
+    }
+
     // Updates the score display
     void UpdateScoreText()
     {
         scoreText.text = "Score: " + score.ToString();
+    }
+
+    void UpdateMissesText()
+    {
+        missesText.text = "Misses: " + misses.ToString();
     }
 }

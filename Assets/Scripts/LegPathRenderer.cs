3ca1a01d4c1d5f35b0da3767ec245c0ede2acf87
Daniel Ding <ddin0018@student.monash.edu>
2024-05-19 22:15:48 +1000

    Try a curved bezier line for the soccer line

---
+++ Assets/Scripts/LegPathRenderer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class LegPathRenderer : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+    private int lineSegments = 60;
+    public GameObject leg;
+    public string fileName = "positions.csv"; // Name of the output file
+    public Vector3[] positions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExportPositionsToCSV();
+   }
+
+    public void ExportPositionsToCSV()
+    {
+        string filePath = Path.Combine(Application.dataPath, fileName);
+        bool fileExists = File.Exists(filePath);
+
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+
+        {
+            // Write the header
+            if (!fileExists)
+            {
+                writer.WriteLine("ObjectName,PositionX,PositionY,PositionZ");
+            }
+
+            Vector3 pos = leg.transform.position;
+            string line = $"{leg.name},{pos.x},{pos.y},{pos.z}";
+            writer.WriteLine(line);
+        }
+
+        Debug.Log($"Positions exported to {filePath}");
+    }
+}
